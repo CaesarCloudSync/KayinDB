041ac728493579e4b73b32cb7fd05116fe836afb
amari <amari.lawal@gmail.com>
2024-03-14 19:23:56 +0000

    improve routing

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "google_cloud_run_service" "run_service" {
   template {
     spec {
       containers {
-        image = "palondomus/kayindb:2"
+        image = "palondomus/kayindb:3"
       }
     }
   }
